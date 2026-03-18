bf9082214112b8c866fbdc4850c41a8fd0731bc6
rakib8680 <rakib.cst.1st@gmail.com>
2026-03-18 15:57:40 +0600

    feat: setup react-router v7 and daisyui
    - Added react-router (v7) for application routing
    - Configured RouterProvider in main.jsx
    - Integrated Tailwind CSS v4 and DaisyUI
    - Set up initial App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div className="text-3xl font-bold underline">
       <h1>This is App component</h1>
+      <button className="btn">Button</button>
     </div>
   );
 };
